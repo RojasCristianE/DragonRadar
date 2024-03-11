f608f580f443223a254fac9f320fe757386a3d63
RojasCristianE <cristianrg333@gmail.com>
2024-03-10 19:07:02 -0600

    bolas

--- screens/Radar.tsx
+++ screens/Radar.tsx
@@ -1,15 +1,96 @@
-import { View } from "react-native";
-import MapLibreGL from '@maplibre/maplibre-react-native';
+import { useEffect, useState } from "react";
+import { Alert, View } from "react-native";
+import MapLibreGL, { MapView, Camera, UserTrackingMode, UserLocation, MarkerView, PointAnnotation } from '@maplibre/maplibre-react-native';
+import Geolocation from '@react-native-community/geolocation';
+import { Text } from "react-native-paper";
 
 MapLibreGL.setAccessToken(null);
 
-const stylesUrl = `http://192.168.1.127:8000/style.json`
+const stylesUrl = 'https://raw.githubusercontent.com/RojasCristianE/DragonRadar/main/style.json'
 
-export default () => (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-        <MapLibreGL.MapView
-            style={{ width: "100%", height: "100%" }}
-            styleURL={stylesUrl}
-        />
-    </View>
-);
+type Location = {
+    accuracy: number;
+    altitude: number;
+    heading: number;
+    latitude: number;
+    longitude: number;
+    speed: number;
+}
+
+const locations = [
+    [-86.276300, 12.123592],
+    [-86.268994, 12.134623],
+    [-86.274447, 12.155949],
+    [-86.286633, 12.145089],
+    [-86.295352, 12.128051],
+    [-86.282400, 12.165687],
+    [-86.225057, 12.134205]
+]
+
+export default () => {
+    const [location, setLocation] = useState<Location | null>(null);
+
+    useEffect(() => {
+        Geolocation.getCurrentPosition(
+            ({ coords }) => {
+                setLocation(coords);
+            },
+            err => Alert.alert('getCurrentPosition Error', JSON.stringify(err)),
+            { enableHighAccuracy: true }
+        );
+
+        const watchId = Geolocation.watchPosition(
+            ({ coords }) => {
+                setLocation(coords);
+            },
+            err => Alert.alert('GetCurrentPosition Error', JSON.stringify(err)),
+            {
+                interval: 5000,
+                enableHighAccuracy: true,
+                useSignificantChanges: true
+            }
+        );
+
+        return () => Geolocation.clearWatch(watchId);
+    }, []);
+
+    return (
+        <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+            {
+                location != null ?
+                    <MapView
+                        style={{ width: "100%", height: "100%" }}
+                        styleURL={stylesUrl}
+                        attributionEnabled={false}
+                    >
+                        <Camera
+                            zoomLevel={11}
+                            centerCoordinate={[location.longitude, location.latitude]}
+                            followUserLocation={true}
+                            followUserMode={UserTrackingMode.FollowWithHeading}
+                        />
+                        <UserLocation
+                            renderMode='native'
+                            androidRenderMode="compass"
+                            visible={true}
+                            animated={true}
+                        />
+                        {
+                            locations.map((location, index) => (
+                                <PointAnnotation
+                                    key={`dragon-ball-${index}`}
+                                    id={`dragon-ball-${index}`}
+                                    coordinate={location}
+                                >
+                                    <View>
+                                        <Text>🟡</Text>
+                                    </View>
+                                </PointAnnotation>
+                            ))
+                        }
+                    </MapView> :
+                    <Text>Cargando...</Text>
+            }
+        </View>
+    )
+};
