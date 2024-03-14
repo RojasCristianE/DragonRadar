e886197594a5f5a05262d4b6b4c660fed8438f83
WebmasterViva <webmaster@vivanicaragua.com.ni>
2024-03-14 15:23:31 -0600

    Actualizar reproducción de sonido en ButtonsContainer.tsx

--- components/radar/overlay/ButtonsContainer.tsx
+++ components/radar/overlay/ButtonsContainer.tsx
@@ -7,31 +7,65 @@ type Props = {
     zoomOut: () => void;
 };
 
-export default ({ zoomIn, zoomOut }: Props) => (
-    <View style={{ position: 'absolute', bottom: 50, left: 50, flex: 1, flexDirection: "row" }}>
-        <TouchableOpacity
-            style={{
-                padding: 10,
-                backgroundColor: '#353535EE',
-                borderTopLeftRadius: 50,
-                borderBottomLeftRadius: 50,
-            }}
-            onPress={() => zoomOut()}
-        >
-            <Icon source="arrow-collapse" color={'#D9D9D9'} size={30} />
-        </TouchableOpacity>
-        <Divider bold={true} />
-        <Divider bold={true} />
-        <TouchableOpacity
-            style={{
-                padding: 10,
-                backgroundColor: '#353535EE',
-                borderTopRightRadius: 50,
-                borderBottomRightRadius: 50,
-            }}
-            onPress={() => zoomIn()}
-        >
-            <Icon source="arrow-expand" color={'#D9D9D9'} size={30} />
-        </TouchableOpacity>
-    </View>
-);
+Sound.setCategory('Playback');
+
+const audio = new Sound(
+    'https://raw.githubusercontent.com/RojasCristianE/DragonRadar/main/start.mp3',
+    '',
+    error => {
+        if (error) {
+            console.log('failed to load the sound', error);
+            return;
+        }
+        console.log('Duración en segundos: ' + audio.getDuration() + ', número de canales: ' + audio.getNumberOfChannels());
+    }
+)
+
+export default ({ zoomIn, zoomOut }: Props) => {
+    const playSound = () => {
+        if (audio.isPlaying()) {
+            audio.stop();
+        }
+
+        audio.play();
+    };
+
+    return (
+        <View style={{ position: 'absolute', bottom: 50, left: 50, flex: 1, flexDirection: "row" }}>
+            <TouchableOpacity
+                style={{
+                    padding: 10,
+                    backgroundColor: '#353535EE',
+                    borderTopLeftRadius: 50,
+                    borderBottomLeftRadius: 50,
+                }}
+                onPress={
+                    () => {
+                        zoomOut()
+                        playSound()
+                    }
+                }
+            >
+                <Icon source="arrow-collapse" color={'#D9D9D9'} size={30} />
+            </TouchableOpacity>
+            <Divider bold={true} />
+            <Divider bold={true} />
+            <TouchableOpacity
+                style={{
+                    padding: 10,
+                    backgroundColor: '#353535EE',
+                    borderTopRightRadius: 50,
+                    borderBottomRightRadius: 50,
+                }}
+                onPress={
+                    () => {
+                        zoomIn()
+                        playSound()
+                    }
+                }
+            >
+                <Icon source="arrow-expand" color={'#D9D9D9'} size={30} />
+            </TouchableOpacity>
+        </View>
+    )
+};
